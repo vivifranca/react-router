dafbdfecba0c3446b0f11c8e3ad72ea9337cf2b9
Viviane Santos França <vfranca@junglescout.com>
2021-12-10 12:38:27 -0300

    1st lesson

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './assets/css/base/base.css'
+import Home from './paginas/Home'
+import Sobre from './paginas/Sobre'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
   <>
-
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/about'>
+          <Sobre />
+        </Route>
+      </Switch>
+    </Router>
   </>
   )
 }
